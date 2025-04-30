1e18b0dea99047daae80099b0311a1965c20b294
Your Name <you@example.com>
2025-04-30 12:52:18 +0530

    update mysql database

--- sms/settings.py
+++ sms/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'angelschooldayalachak',
+        'USER': 'angelschool',
+        'PASSWORD': 'Qdqm395#0',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
+
 
 
 # Password validation
